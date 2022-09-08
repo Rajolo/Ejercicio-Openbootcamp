b382b0a650f1217fd971aa799a30ba51d3caef5c
rajolo <rafaeljoseloes@gmail.com>
2022-09-07 21:30:44 -0400

    1er ejercicio funciones

---
+++ primero_funciones.py
@@ -0,0 +1,26 @@
+def area_triangulo(b,h):
+
+    r = (b * h) / 2
+    print("El area del triangulo es: ",r)
+
+area_triangulo(8,7)
+
+print("¡Felicitaciones!, calculaste el area de un triangulo.")
+
+print("Siguiente ejercicio")
+
+"""========================================="""
+import math
+p = math.pi
+     
+
+def area_circulo(r):
+    a = (p * (r**2))
+    print("El area de circulo es: " ,round(a,4))
+
+area_circulo(2)
+
+
+print("Felicitaciones!, calculaste el area de un circulo.")
+
+print("Hasta luego")
